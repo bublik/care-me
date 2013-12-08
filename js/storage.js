a0930828c4151ff8aff45c0be13bdd17843e3da5
Ruslan Voloshin <rebisall@gmail.com>
2013-12-08 21:34:51 +0200

    Added example application with 3 category and JSON based items

---
+++ js/storage.js
@@ -0,0 +1,37 @@
+var CompaniesStorage = function () {
+  return {
+    "keys": {
+      companies: [
+        {img_path: 'img/companies/company1.png',
+          company_name: "Keys 1",
+          description: "Descriptio This is my first post!",
+          phones: ['0923234234234', '234234234435', '234234234234']
+        },
+        {img_path: 'img/companies/company2.png',
+          company_name: "Keys 2",
+          description: "Description This is my first post!",
+          phones: ['234234234435', '234234234234']
+        }]
+    },
+    "gai": {
+      companies: [
+        {img_path: 'img/companies/company1.png',
+          company_name: "company1",
+          description: "Descriptio This is my first post!",
+          phones: ['0923234234234', '234234234435', '234234234234']
+        },
+        {img_path: 'img/companies/company2.png',
+          company_name: "company2",
+          description: "Description This is my first post!",
+          phones: ['234234234435', '234234234234']
+        },
+        {img_path: 'img/companies/company4.png',
+          company_name: "company4",
+          description: "Descriptio This is my first post!",
+          phones: ['09232342344', '234234234435']
+        }
+      ]
+    }
+  }
+
+}
